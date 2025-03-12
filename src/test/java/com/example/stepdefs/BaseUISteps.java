6761e07ff7959d7f7847fc475c0c0157863637b5
surajbhatti <a.suraj@outlook.com>
2025-03-12 19:02:49 +0530

    Added UiActions initialization to BaseUISteps for improved UI test handling

--- src/test/java/com/example/stepdefs/BaseUISteps.java
+++ src/test/java/com/example/stepdefs/BaseUISteps.java
@@ -3,9 +3,11 @@ package com.example.stepdefs;
 import com.example.pages.PageFactory;
 import com.example.enums.TestType;
 import com.example.context.TestContext;
+import com.example.actions.UiActions;
 
 public class BaseUISteps {
     protected final PageFactory pages;
+    protected final UiActions uiActions;
     
     public BaseUISteps() {
         TestContext testContext = Hooks.getTestContext();
@@ -13,5 +15,6 @@ public class BaseUISteps {
             throw new IllegalStateException("UI Steps cannot be used with non-UI tests");
         }
         this.pages = testContext.getPages();
+        this.uiActions = testContext.getPage();
     }
 } 
